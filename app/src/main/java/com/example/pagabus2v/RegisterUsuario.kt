037f6da156bcfe87da70eca26599a9788e0800ce
IngBubu <l20170623@culiacan.tecnm.mx>
2024-03-05 00:12:48 -0700

    Haciendo y configurando autenticacion login y register

--- app/src/main/java/com/example/pagabus2v/RegisterUsuario.kt
+++ app/src/main/java/com/example/pagabus2v/RegisterUsuario.kt
@@ -2,10 +2,13 @@ package com.example.pagabus2v
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RegisterUsuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_usuario)
     }
+
+    fun goTerms(view: View) {}
 }
